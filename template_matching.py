2428b64cfffd44ffedd604066d532d241058e964
Tomáš Orviský <orvisky.tomas@centrum.cz>
2024-04-23 22:51:50 +0200

    prange

--- template_matching.py
+++ template_matching.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numba import njit
+from numba import njit, prange
 
 
 @njit("Tuple((int64,int64))(float64[:,:,:], float64[:,:,:], int64)", parallel=True, cache=True)
@@ -10,21 +10,30 @@ def normalized_cross_correlation(image: np.ndarray, template: np.ndarray, step: 
     shape = (img_height - templ_height) // step + 1, (img_width - templ_width) // step + 1
     template_sum_sqrt = np.sum(template ** 2)
 
-    max_val = 0.0
-    pos = 0, 0
-    for x in range(shape[0]):
+    max_result = np.zeros((image.shape[0],), dtype=np.float64)
+    pos_x = np.zeros((image.shape[0],), dtype=np.int64)
+    pos_y = np.zeros((image.shape[0],), dtype=np.int64)
+
+    for x in prange(shape[0]):
+        local_max = 0.0
+        local_pos = (0, 0)
+
         x_step = x * step
         x_step_templ_height = x_step + templ_height
 
-        for y in range(shape[1]):
+        for y in prange(shape[1]):
             window = image[x_step:x_step_templ_height, y * step:y * step + templ_width]
 
-            val = np.sum(window * template) / np.sqrt(np.sum(window ** 2) * template_sum_sqrt)
-            if val > max_val:
-                pos = y * step, x_step
-                max_val = val
+            val = np.sum(template * window) / np.sqrt(template_sum_sqrt * np.sum(window ** 2))
+            if val > local_max:
+                local_pos = x_step, y * step
+                local_max = val
 
-    return pos
+        max_result[x] = local_max
+        pos_x[x], pos_y[x] = local_pos
+
+    final_idx = np.argmax(max_result)
+    return pos_y[final_idx], pos_x[final_idx]
 
 
 def rgb2gray(matrix):
