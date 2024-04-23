7ff098ea7dcb188899450f91a9facc33ca5b3c03
Tomáš Orviský <orvisky.tomas@centrum.cz>
2024-04-23 22:21:08 +0200

    no cython, simplified formula

--- template_matching.py
+++ template_matching.py
@@ -7,33 +7,24 @@ def normalized_cross_correlation(image: np.ndarray, template: np.ndarray, step: 
     img_height, img_width, img_channels = image.shape
     templ_height, templ_width, templ_channels = template.shape
 
-    shape = ((img_height - templ_height) // step + 1, (img_width - templ_width) // step + 1)
-    template_mean = np.mean(template)
-    template_diff = template - template_mean
-    template_sum_sqrt = np.sqrt(np.sum((template - template_mean) ** 2))
+    shape = (img_height - templ_height) // step + 1, (img_width - templ_width) // step + 1
+    template_sum_sqrt = np.sum(template ** 2)
 
-    max_result = 0.0
-    max_x = max_y = 0
+    max_val = 0.0
+    pos = 0, 0
     for x in range(shape[0]):
         x_step = x * step
         x_step_templ_height = x_step + templ_height
 
         for y in range(shape[1]):
             window = image[x_step:x_step_templ_height, y * step:y * step + templ_width]
-            window_mean = np.mean(window)
-            window_diff = window - window_mean
 
-            numerator = np.sum(window_diff * template_diff)
-            denominator = np.sqrt(np.sum(window_diff ** 2) * template_sum_sqrt)
+            val = np.sum(window * template) / np.sqrt(np.sum(window ** 2) * template_sum_sqrt)
+            if val > max_val:
+                pos = y * step, x_step
+                max_val = val
 
-            result = numerator / denominator
-
-            if result > max_result:
-                max_x = x_step
-                max_y = y * step
-                max_result = result
-
-    return max_y, max_x
+    return pos
 
 
 def rgb2gray(matrix):
